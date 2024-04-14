e8c851dc33578c1d6b227610f67d41f1a6dd774d
StafLoker <staf.loker@gmail.com>
2024-04-14 03:19:53 +0200

    refactor: pages

--- firmware/IntelIrrigation/pages.h
+++ firmware/IntelIrrigation/pages.h
@@ -1,3 +1,32 @@
 #pragma once
 
+/*
+  -- pages.h --
+  Include grafic of pages, not logic.
+*/
+
+// Timers
+#define DISPLAY_TIMER 30
+uint32_t displayTimer;
+
+// Page delays
+#define LOGO_PAGE_DELAY 2000
+#define CONGRATULATION_INITIAL_CONFIGURATION_PAGE_DELAY 1300
+
+// -------------
+// -- Headers --
+// -------------
+
 void viewLogoPage();
+void viewEnterPower();
+void viewEnterPowerRange(uint16_t, uint16_t);
+void viewEnterMlLiquid();
+void viewChooseMode();
+void viewEnterSchedule();
+void viewCongratulationsInitialConfiguration();
+void viewMainMenu();
+void viewReview();
+void viewManualMode(bool);
+void viewSettings();
+void viewSettingsAutoMode();
+void viewSettingsScheduleMode();
