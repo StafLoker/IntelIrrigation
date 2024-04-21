f0bcde9d50dcb29e219531d9a14a7b844ca16839
StafLoker <staf.loker@gmail.com>
2024-04-21 13:30:32 +0200

    refactor: improve drawButton function of UIComponents

--- firmware/IntelIrrigation/UIComponents.ino
+++ firmware/IntelIrrigation/UIComponents.ino
@@ -2,13 +2,5 @@
 
 void drawButton(uint8_t x, uint8_t y, char *text, bool selected)
 {
-    // Need to Test: display.drawButtonUTF8(x, y, selected ? U8G2_BTN_BW1 | U8G2_BTN_INV : U8G2_BTN_BW1, 0, 2, 2, text);
-    if (selected)
-    {
-        display.drawButtonUTF8(x, y, U8G2_BTN_BW1 | U8G2_BTN_INV, 0, 2, 2, text);
-    }
-    else
-    {
-        display.drawButtonUTF8(x, y, U8G2_BTN_BW1, 0, 2, 2, text);
-    }
+    display.drawButtonUTF8(x, y, selected ? U8G2_BTN_BW1 | U8G2_BTN_INV : U8G2_BTN_BW1, 0, 2, 2, text);
 }
