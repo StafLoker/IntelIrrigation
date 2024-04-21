ad0c83fa481b687333914eccde9c17a0b9e04df3
StafLoker <staf.loker@gmail.com>
2024-04-21 12:42:25 +0200

    feat: drawButton to UIComponents

--- firmware/IntelIrrigation/UIComponents.h
+++ firmware/IntelIrrigation/UIComponents.h
@@ -1,1 +1,7 @@
 #pragma once
+
+// -------------
+// -- Headers --
+// -------------
+
+void drawButton(uint8_t x, uint8_t y, char *text, bool selected);
--- firmware/IntelIrrigation/UIComponents.ino
+++ firmware/IntelIrrigation/UIComponents.ino
@@ -1,1 +1,14 @@
 #include "global.h"
+
+void drawButton(uint8_t x, uint8_t y, char *text, bool selected)
+{
+    // Need to Test: display.drawButtonUTF8(x, y, selected ? U8G2_BTN_BW1 | U8G2_BTN_INV : U8G2_BTN_BW1, 0, 2, 2, text);
+    if (selected)
+    {
+        display.drawButtonUTF8(x, y, U8G2_BTN_BW1 | U8G2_BTN_INV, 0, 2, 2, text);
+    }
+    else
+    {
+        display.drawButtonUTF8(x, y, U8G2_BTN_BW1, 0, 2, 2, text);
+    }
+}
